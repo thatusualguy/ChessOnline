d439f776ea129533e503addeb0cfc42fa4718733
thatusualguy <m.p.rybakov@gmail.com>
2022-12-01 14:08:29 +0300

    init

---
+++ app/src/main/java/io/github/thatusualguy/chessonline/ui/MainActivity.java
@@ -0,0 +1,16 @@
+package io.github.thatusualguy.chessonline.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import io.github.thatusualguy.chessonline.R;
+
+public class MainActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
