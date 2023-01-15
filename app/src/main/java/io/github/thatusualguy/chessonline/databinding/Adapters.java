084be29116c0d851d190faae8030536a68d2684a
thatusualguy <m.p.rybakov@gmail.com>
2023-01-16 02:09:34 +0300

    code cleanup

--- app/src/main/java/io/github/thatusualguy/chessonline/databinding/Adapters.java
+++ app/src/main/java/io/github/thatusualguy/chessonline/databinding/Adapters.java
@@ -6,7 +6,7 @@ import android.widget.TextView;
 import androidx.databinding.BindingAdapter;
 
 public class Adapters {
-	@BindingAdapter(value = "app:drawableTint") //customise your name here
+	@BindingAdapter(value = "drawableTint") //customise your name here
 	public static void setTextColor(TextView view, int color) {
 		Drawable a = view.getCompoundDrawables()[0];
 		if (a != null)
