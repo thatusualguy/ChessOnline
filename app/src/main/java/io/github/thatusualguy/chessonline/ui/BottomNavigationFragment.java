2e858ba879764786bc2b0499152a192834d9368c
thatusualguy <m.p.rybakov@gmail.com>
2023-01-16 02:10:16 +0300

    fix bottom navigation

--- app/src/main/java/io/github/thatusualguy/chessonline/ui/BottomNavigationFragment.java
+++ app/src/main/java/io/github/thatusualguy/chessonline/ui/BottomNavigationFragment.java
@@ -2,65 +2,66 @@ package io.github.thatusualguy.chessonline.ui;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import io.github.thatusualguy.chessonline.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link BottomNavigationFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import io.github.thatusualguy.chessonline.R;
+import io.github.thatusualguy.chessonline.databinding.BottomNavigationLayoutBinding;
+import io.github.thatusualguy.chessonline.databinding.FragmentBottomNavigationBinding;
+import io.github.thatusualguy.chessonline.databinding.FragmentProfileBinding;
+
 public class BottomNavigationFragment extends Fragment {
 
-	// TODO: Rename parameter arguments, choose names that match
-	// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-	private static final String ARG_PARAM1 = "param1";
-	private static final String ARG_PARAM2 = "param2";
 
-	// TODO: Rename and change types of parameters
-	private String mParam1;
-	private String mParam2;
+	private FragmentBottomNavigationBinding binding;
 
 	public BottomNavigationFragment() {
 		// Required empty public constructor
 	}
 
-	/**
-	 * Use this factory method to create a new instance of
-	 * this fragment using the provided parameters.
-	 *
-	 * @param param1 Parameter 1.
-	 * @param param2 Parameter 2.
-	 * @return A new instance of fragment BottomNavigationFragment.
-	 */
-	// TODO: Rename and change types and number of parameters
-	public static BottomNavigationFragment newInstance(String param1, String param2) {
+	public static BottomNavigationFragment newInstance() {
 		BottomNavigationFragment fragment = new BottomNavigationFragment();
-		Bundle args = new Bundle();
-		args.putString(ARG_PARAM1, param1);
-		args.putString(ARG_PARAM2, param2);
-		fragment.setArguments(args);
 		return fragment;
 	}
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		if (getArguments() != null) {
-			mParam1 = getArguments().getString(ARG_PARAM1);
-			mParam2 = getArguments().getString(ARG_PARAM2);
-		}
 	}
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 							 Bundle savedInstanceState) {
 		// Inflate the layout for this fragment
-		return inflater.inflate(R.layout.fragment_bottom_navigation, container, false);
+		binding = FragmentBottomNavigationBinding.inflate(inflater, container, false);
+		View view = binding.getRoot();
+		return view;
+	}
+
+	@Override
+	public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+		super.onViewCreated(view, savedInstanceState);
+
+		NavigationUI.setupWithNavController(
+				binding.profileBottomNavigation,
+				Navigation.findNavController(binding.getRoot())
+		);
+	}
+
+	@Override
+	public void onDestroyView() {
+		super.onDestroyView();
+		binding = null;
 	}
 }
